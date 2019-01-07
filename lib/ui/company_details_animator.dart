30fa0ef20bbaa16251cdbaf152e2a5a13f5af936
William Martins <owilliam.net@gmail.com>
2019-01-07 20:17:02 -0200

    First commit. App for test animations on Flutter

---
+++ lib/ui/company_details_animator.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CompanyDetailsAnimator extends StatefulWidget {
+  @override
+  _CompanyDetailsAnimatorState createState() => _CompanyDetailsAnimatorState();
+}
+
+class _CompanyDetailsAnimatorState extends State<CompanyDetailsAnimator>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 1780),
+      vsync: this);
+
+      _controller.forward();
+  }
+
+
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Hello World"),
+      ),
+    );
+  }
+}
